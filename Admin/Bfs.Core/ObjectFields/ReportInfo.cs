3d6adb91076d2daf0cf3255d35250cb21e5d110b
AlphaOmegaFirstLast <xx@yy.zz>
2026-02-12 16:51:54 +1100

    Modify ReportInfo structure

--- Admin/Bfs.Core/ObjectFields/ReportInfo.cs
+++ Admin/Bfs.Core/ObjectFields/ReportInfo.cs
@@ -2,6 +2,8 @@
 
 public class ReportInfo 
 {
+    public bool? IsQueryColumn { get; set; } = true;
+    public bool? IsColumnVisible { get; set; } = true;
     public string? ParentTable { get; set; } = string.Empty;
     public bool? IsJoinField { get; set; } = false;
     public int? AggregateTypeId { get; set; } = 0;
