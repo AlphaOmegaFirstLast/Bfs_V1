ed62bed8b8995c589bcc663d995d431acc584aff
AlphaOmegaFirstLast <xx@yy.zz>
2026-03-06 13:52:19 +1100

    update app layout

--- V1/Frontend/main/src/app/layouts/components/sidenav/sidenav.component.ts
+++ V1/Frontend/main/src/app/layouts/components/sidenav/sidenav.component.ts
@@ -1,19 +1,15 @@
 import {Component} from '@angular/core';
-import {UserProfileComponent} from '@layouts/components/sidenav/components/user-profile/user-profile.component';
 import {AppMenuComponent} from '@layouts/components/sidenav/components/app-menu/app-menu.component';
 import {SimplebarAngularModule} from 'simplebar-angular';
 import {LayoutStoreService} from '@core/services/layout-store.service';
-import {NgIf} from '@angular/common';
 import {NgIcon} from '@ng-icons/core';
 import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
     imports: [
-        UserProfileComponent,
         AppMenuComponent,
         SimplebarAngularModule,
-        NgIf,
         NgIcon,
         RouterLink
     ],
