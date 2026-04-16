d2bc63183ce84b4ec3c1d976632b1b9adccac7d3
AlphaOmegaFirstLast <xx@yy.zz>
2026-04-16 15:29:36 +1000

    **Frontend: Remove "home" routes from main\routes

--- V1/Frontend/main/src/bfs/auth-main/auth.route.ts
+++ V1/Frontend/main/src/bfs/auth-main/auth.route.ts
@@ -3,14 +3,6 @@ import {Routes} from '@angular/router';
 export const Auth_ROUTES: Routes = [
     {
         path: '',
-        loadChildren: () => import('../home/home.route').then((mod) => mod.Home_ROUTES),
-    },
-    {
-        path: '',
-        loadChildren: () => import('../error/error.route').then((mod) => mod.ERROR_PAGES_ROUTES)
-    },
-    {
-        path: '',
         loadChildren: () => import('../auth-tables/tables.route').then((mod) => mod.TABLES_ROUTES),
     },
     {
@@ -22,3 +14,4 @@ export const Auth_ROUTES: Routes = [
         loadChildren: () => import('../auth-seed/seed.route').then((mod) => mod.Seed_ROUTES),
     }   
 ];
+
