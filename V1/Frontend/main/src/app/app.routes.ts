1cbd3977729c906a32a7980286bb7a5c90ea278e
AlphaOmegaFirstLast <xx@yy.zz>
2026-04-16 15:16:39 +1000

    **Frontend: Fix double execution of a component. <router-outlet/> shoould be called once.

--- V1/Frontend/main/src/app/app.routes.ts
+++ V1/Frontend/main/src/app/app.routes.ts
@@ -1,50 +1,45 @@
-import {Routes} from '@angular/router';
-import {MainLayoutComponent} from '@layouts/main-layout/main-layout.component';
-import { HomeComponent } from '../bfs/home/home.component';
-import { environment } from '@environment/environment.staging';
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from '@layouts/main-layout/main-layout.component';
+// import { HomeComponent } from '../bfs/home/home.component';
+// import { environment } from '@environment/environment.staging';
 
 const BFS_SYSTEMS_ROUTES: Routes = [
     {
         path: '',
         redirectTo: 'home',
         pathMatch: 'full',
-
-    },
-    {
-        path: 'landing',
-        component: HomeComponent,
     },
     {
         path: '',
         component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/home/home.route').then((mod) => mod.Home_ROUTES),
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('../bfs/home/home.route').then((mod) => mod.Home_ROUTES),
+            },
+            {
+                path: '',
+                loadChildren: () => import('../bfs/error/error.route').then((mod) => mod.ERROR_PAGES_ROUTES)
+            },
+            {
+                path: '',
+                loadChildren: () => import('../bfs/infrastructure-main/infrastructure.route').then((mod) => mod.Infrastructure_ROUTES)
+            },
+            {
+                path: '',
+                loadChildren: () => import('../bfs/stores-main/stores.route').then((mod) => mod.Stores_ROUTES)
+            },
+            {
+                path: '',
+                loadChildren: () => import('../bfs/auth-main/auth.route').then((mod) => mod.Auth_ROUTES)
+            },
+            {
+                path: '',
+                loadChildren: () => import('../bfs/master-main/master.route').then((mod) => mod.Master_ROUTES)
+            },
+            //Template_System_AddRouteEntry
+        ]
     },
-    {
-        path: '',
-        component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/error/error.route').then((mod) => mod.ERROR_PAGES_ROUTES)
-    },
-    {
-        path: '',
-        component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/infrastructure-main/infrastructure.route').then((mod) => mod.Infrastructure_ROUTES)
-    },
-    {
-        path: '',
-        component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/stores-main/stores.route').then((mod) => mod.Stores_ROUTES)
-    },
-    {
-        path: '',
-        component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/auth-main/auth.route').then((mod) => mod.Auth_ROUTES)
-    },
-    {
-        path: '',
-        component: MainLayoutComponent,
-        loadChildren: () => import('../bfs/master-main/master.route').then((mod) => mod.Master_ROUTES)
-    },
-//Template_System_AddRouteEntry
 ];
 
 export const routes: Routes = BFS_SYSTEMS_ROUTES;
--- V1/Frontend/main/src/app/layouts/main-layout/main-layout.component.ts
+++ V1/Frontend/main/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {VerticalLayoutComponent} from '@layouts/vertical-layout/vertical-layout.component';
-import {RouterOutlet} from '@angular/router';
 import {HorizontalLayoutComponent} from '@layouts/horizontal-layout/horizontal-layout.component';
 import {LayoutStoreService} from '@core/services/layout-store.service';
 
@@ -8,9 +7,7 @@ import {LayoutStoreService} from '@core/services/layout-store.service';
     selector: 'app-main-layout',
     imports: [
         VerticalLayoutComponent,
-        RouterOutlet,
         HorizontalLayoutComponent,
-
     ],
     templateUrl: './main-layout.component.html',
 })
